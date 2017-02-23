65583f6e19e027ca8ec78da5fe77982a0de5719c
VersionBeathon <s875766683@hotmail.com>
2017-02-23 16:43:40 +0800

    Init

--- chapt_9/AuthorshipAnalysis.py
+++ chapt_9/AuthorshipAnalysis.py
@@ -11,9 +11,15 @@ from sklearn.cross_validation import cross_val_score
 from sklearn.pipeline import Pipeline
 from sklearn import grid_search
 from sklearn.feature_extraction.text import CountVectorizer
-
+from email.parser import Parser
+from sklearn.utils import check_random_state
+from sklearn.cross_validation import train_test_split
+from sklearn.metrics import confusion_matrix
+from matplotlib import pyplot as plt
+import quotequail
 
 data_folder = r"C:\Users\Administrator\Data\books"
+
 
 def clean_book(document):
     lines = document.split("\n")
@@ -96,3 +102,75 @@ grid = grid_search.GridSearchCV(svr, parameters)
 pipeline1 = Pipeline([('feature_extraction', extractor), ('clf', grid)])
 pipeline = Pipeline([('feature_extraction', CountVectorizer(analyzer='char', ngram_range=(3, 3))),
                     ('classifier', grid)])
+
+p = Parser()
+
+
+def get_enron_corpus(num_authors=10, data_folder=data_folder,
+                     min_docs_author=10, max_docs_author=100,
+                     random_state=None):
+    random_state = check_random_state(random_state)
+    email_addresses = sorted(os.listdir(data_folder))
+    random_state.shuffle(email_addresses)
+    documents = []
+    classes = []
+    author_num = 0
+    authors = {}
+    for user in email_addresses:
+        users_email_folder = os.path.join(data_folder, user)
+        mail_folders = [os.path.join(users_email_folder, subfolder)
+                        for subfolder in os.listdir(users_email_folder)
+                        if "sent" in subfolder]
+        try:
+            authored_emails = [open(os.path.join(mail_folder, email_filename), encoding='cp1252').read()
+                               for mail_folder in mail_folders
+                               for email_filename in os.listdir(mail_folder)]
+        except IsADirectoryError:
+            continue
+        if len(authored_emails) < min_docs_author:
+            continue
+        if len(authored_emails) > max_docs_author:
+            authored_emails = authored_emails[:max_docs_author]
+        contents = [p.parsestr(email)._payload for email in authored_emails]
+        documents.extend(contents)
+        classes.extend([author_num] * len(authored_emails))
+        authors[user] = author_num
+        author_num += 1
+        if author_num >= num_authors or author_num >= len(email_addresses):
+            break
+    return documents, np.array(classes), authors
+
+enron_data_folder = r'H:\learning\Python数据挖掘入门与实战\Code_REWRITE\Chapter 9\maildir'
+documents, classes, authors = get_enron_corpus(data_folder=enron_data_folder, random_state=14)
+print(documents[100])
+
+
+def remove_replies(email_contents):
+    r = quotequail.unwrap(email_contents)
+    if r is None:
+        return email_contents
+    if 'text_top' in r:
+        return r['text_top']
+    elif 'text' in r:
+        return r['text']
+    return email_contents
+
+documents = [remove_replies(document) for document in documents]
+
+training_documents, testing_documents, y_train, y_test = train_test_split(documents, classes, random_state=14)
+pipeline.fit(training_documents, y_train)
+y_pred = pipeline.predict(testing_documents)
+print(pipeline.named_steps['classifier'].best_params_)
+
+cm = confusion_matrix(y_pred, y_test)
+cm = cm / cm.astype(np.float).sum(axis=1)
+sorted_authors = sorted(authors.keys(), key=lambda x:authors[x])
+
+plt.figure(figsize=(100, 100))
+plt.imshow(cm, cmap='Blues')
+tick_marks = np.arange(len( sorted_authors ))
+plt.xticks(tick_marks, sorted_authors )
+plt.yticks(tick_marks, sorted_authors )
+plt.ylabel('Actual')
+plt.xlabel('Predicted')
+plt.show()
